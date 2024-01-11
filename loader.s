fd54564ef8c09d2b689d04967cd3bf9dee36c729
gmmorgan1996 <griffin.mrogan@gmail.com>
2024-01-10 19:59:08 -0500

    test

--- loader.s
+++ loader.s
@@ -23,3 +23,5 @@ section .bss
 align 4                                     ; align at 4 bytes
 kernel_stack:                               ; label points to beginning of memory
     resb KERNEL_STACK_SIZE                  ; reserve stack for the kernel
+    mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the
+                                                ; stack (end of memory area)
