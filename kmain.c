684e08974c5921f11ac8bc50689b4a09af0a10e8
gmmorgan1996 <griffin.mrogan@gmail.com>
2024-01-12 15:03:18 -0500

    test

--- kmain.c
+++ kmain.c
@@ -8,6 +8,27 @@
 /* The I/O port commands */
 #define FB_HIGH_BYTE_COMMAND 14
 #define FB_LOW_BYTE_COMMAND 15
+/* The I/O ports */
+/* All the I/O ports are calculated relative to the data port. This is because
+* all serial ports (COM1, COM2, COM3, COM4) have their ports in the same
+* order, but they start at different values.
+*/
+#define SERIAL_COM1_BASE 0x3F8 /* COM1 base port */
+#define SERIAL_DATA_PORT(base) (base)
+#define SERIAL_FIFO_COMMAND_PORT(base) (base + 2)
+#define SERIAL_LINE_COMMAND_PORT(base) (base + 3)
+#define SERIAL_MODEM_COMMAND_PORT(base) (base + 4)
+#define SERIAL_LINE_STATUS_PORT(base) (base + 5)
+/* The I/O port commands */
+/* SERIAL_LINE_ENABLE_DLAB:
+* Tells the serial port to expect first the highest 8 bits on the data port,
+* then the lowest 8 bits will follow
+*/
+#define SERIAL_LINE_ENABLE_DLAB 0x80
+/** serial_configure_baud_rate:
+* Sets the speed of the data being sent. The default speed of a serial
+* port is 115200 bits/s. The argument is a divisor of that number, hence
+* the resulting speed becomes (115200 / divisor) bits/s.
 
 void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg)
 {   char *fb = (char *) 0x000B8000;
@@ -25,6 +46,36 @@ void fb_write_cell_two(unsigned int i, char c, unsigned char fg, unsigned char b
 // {
 //     return arg1 + arg2 + arg3;
 // }
+
+*
+* @param com The COM port to configure
+* @param divisor The divisor
+*/
+void serial_configure_baud_rate(unsigned short com, unsigned short divisor)
+{
+    outb(SERIAL_LINE_COMMAND_PORT(com),
+    SERIAL_LINE_ENABLE_DLAB);
+    outb(SERIAL_DATA_PORT(com),
+    (divisor >> 8) & 0x00FF);
+    outb(SERIAL_DATA_PORT(com),
+    divisor & 0x00FF);
+}
+
+/** serial_configure_line:
+* Configures the line of the given serial port. The port is set to have a
+* data length of 8 bits, no parity bits, one stop bit and break control
+* disabled.
+*
+* @param com The serial port to configure
+*/
+void serial_configure_line(unsigned short com)
+{
+    /* Bit: | 7 | 6 | 5 4 3 | 2 | 1 0 |
+    * Content: | d | b | prty | s | dl |
+    * Value: | 0 | 0 | 0 0 0 | 0 | 1 1 | = 0x03
+    */
+    outb(SERIAL_LINE_COMMAND_PORT(com), 0x03);
+}
 
 /** fb_move_cursor:
 * Moves the cursor of the framebuffer to the given position
@@ -70,10 +121,12 @@ void entry(){
     // fb_write_cell(0, 'A', FB_GREEN, FB_DARK_GREY);
     // fb_move_cursor(1);
     // fb_write_cell_two(0, 'B', FB_GREEN, FB_DARK_GREY);
+    serial_configure_baud_rate(SERIAL_COM1_BASE, 2);
+    serial_configure_line(SERIAL_COM1_BASE);
     cls();
     char test[] = "GHOST OS v0.0.0.1";
     write(test, sizeof(test));
-    fb_move_cursor(189);
+    fb_move_cursor(80);
     while(1);
 }
 
