50907e11687eff87164a92ad3d9d7ef29dfec6e2
gmmorgan1996 <griffin.mrogan@gmail.com>
2024-01-12 15:09:39 -0500

    test

--- kmain.c
+++ kmain.c
@@ -116,6 +116,20 @@ void cls(){
     fb_move_cursor(0);
 }
 
+/** serial_is_transmit_fifo_empty:
+* Checks whether the transmit FIFO queue is empty or not for the given COM
+* port.
+*
+* @param com The COM port
+* @return 0 if the transmit FIFO queue is not empty
+* 1 if the transmit FIFO queue is empty
+*/
+int serial_is_transmit_fifo_empty(unsigned int com)
+{
+    /* 0x20 = 0010 0000 */
+    return inb(SERIAL_LINE_STATUS_PORT(com)) & 0x20;
+}
+
 void entry(){
     // fb_move_cursor(0);
     // fb_write_cell(0, 'A', FB_GREEN, FB_DARK_GREY);
@@ -123,6 +137,7 @@ void entry(){
     // fb_write_cell_two(0, 'B', FB_GREEN, FB_DARK_GREY);
     serial_configure_baud_rate(SERIAL_COM1_BASE, 2);
     serial_configure_line(SERIAL_COM1_BASE);
+    serial_is_transmit_fifo_empty(SERIAL_COM1_BASE);
     cls();
     char test[] = "GHOST OS v0.0.0.1";
     write(test, sizeof(test));
