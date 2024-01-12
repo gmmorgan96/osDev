7233838ae4509050a3284c6a3ff1f5b700470091
gmmorgan1996 <griffin.mrogan@gmail.com>
2024-01-12 09:39:45 -0500

    add sum_of_three

--- loader.s
+++ loader.s
@@ -6,7 +6,7 @@ CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
                                 ; (magic number + checksum + flags should equal 0)
 KERNEL_STACK_SIZE equ 4096                  ; size of stack in bytes
 ; The assembly code
-
+external sum_of_three           ; the function sum_of_three is defined elsewhere
 section .text:                  ; start of the text (code) section
 align 4                         ; the code must be 4 byte aligned
     dd MAGIC_NUMBER             ; write the magic number to the machine code,
