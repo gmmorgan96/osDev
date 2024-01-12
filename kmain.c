871ed30dfdaa52719d37e4997de4c0bf97c020df
gmmorgan1996 <griffin.mrogan@gmail.com>
2024-01-12 10:05:22 -0500

    test

--- kmain.c
+++ kmain.c
@@ -2,23 +2,15 @@
 #define FB_GREEN 2
 #define FB_DARK_GREY 8
 
+void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg)
+{   char *fb = (char *) 0x000B8000;
+    fb[i] = c;
+    fb[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
+}
+
 int sum_of_three(int arg1, int arg2, int arg3)
 {
     fb_write_cell(0, "A", FB_GREEN, FB_DARK_GREY);
     return arg1 + arg2 + arg3;
 }
 
-/** fb_write_cell:
-* Writes a character with the given foreground and background to position i
-* in the framebuffer.
-*
-* @param i The location in the framebuffer
-* @param c The character
-* @param fg The foreground color
-* @param bg The background color
-*/
-void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg)
-{   char *fb = (char *) 0x000B8000;
-    fb[i] = c;
-    fb[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
-}
