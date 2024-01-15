97a07b8e46ed2bf0d680d0997a964218e55ae02d
gmmorgan1996 <griffin.mrogan@gmail.com>
2024-01-15 16:00:39 -0500

    text

--- loader.s
+++ loader.s
@@ -27,6 +27,7 @@ align 4                         ; the code must be 4 byte aligned
 loader:                         ; the loader label (defined as entry point in linker script)
     ; mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
         ; call cls
+    push ebx
     call entry ; call the function, the result will be in eax
 
 cls:
