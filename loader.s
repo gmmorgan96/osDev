4ac2639bf9c39731f3471b70c6dd4983ba70d359
gmmorgan1996 <griffin.mrogan@gmail.com>
2024-01-15 14:58:01 -0500

    test

--- loader.s
+++ loader.s
@@ -13,7 +13,7 @@ section .text:                  ; start of the text (code) section
 
 align 4                         ; the code must be 4 byte aligned
     dd MAGIC_NUMBER             ; write the magic number to the machine code,
-    dd ALIGN_MODULES            ; write the align modules instruction
+    ; dd ALIGN_MODULES            ; write the align modules instruction
     dd FLAGS                    ; the flags,
     dd CHECKSUM                 ; and the checksum
     
