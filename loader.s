f3bf599428c34d09240577b38985c7199469fdb1
gmmorgan1996 <griffin.mrogan@gmail.com>
2024-01-15 15:04:58 -0500

    test

--- loader.s
+++ loader.s
@@ -1,6 +1,7 @@
 global loader                   ; the entry symbol for ELF
 
-MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
+; MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
+MAGIC_NUMBER equ 0x1BADB002
 ALIGN_MODULES equ 0x00000001    ; tell GRUB to align modules
 FLAGS        equ 0x0            ; multiboot flags
 CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
@@ -14,7 +15,7 @@ section .text:                  ; start of the text (code) section
 align 4                         ; the code must be 4 byte aligned
     dd MAGIC_NUMBER             ; write the magic number to the machine code,
     dd ALIGN_MODULES            ; write the align modules instruction
-    ; dd FLAGS                    ; the flags,
+    dd FLAGS                    ; the flags,
     dd CHECKSUM                 ; and the checksum
     
 
