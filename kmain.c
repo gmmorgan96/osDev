e15f09bc7519e8d19aa01cca16b1db0c65fb2c9a
gmmorgan1996 <griffin.mrogan@gmail.com>
2024-01-15 15:56:59 -0500

    test

--- kmain.c
+++ kmain.c
@@ -294,12 +294,12 @@ void entry(unsigned int ebx){
     // fb_write_cell(0, 'A', FB_GREEN, FB_DARK_GREY);
     // fb_move_cursor(1);
     // fb_write_cell_two(0, 'B', FB_GREEN, FB_DARK_GREY);
-    serial_configure_baud_rate(SERIAL_COM1_BASE, 2);
-    serial_configure_line(SERIAL_COM1_BASE);
-    serial_is_transmit_fifo_empty(SERIAL_COM1_BASE);
-    cls();
-    char test[] = "GHOST OS v0.0.0.1";
-    write(test, sizeof(test));
+    // serial_configure_baud_rate(SERIAL_COM1_BASE, 2);
+    // serial_configure_line(SERIAL_COM1_BASE);
+    // serial_is_transmit_fifo_empty(SERIAL_COM1_BASE);
+    // cls();
+    // char test[] = "GHOST OS v0.0.0.1";
+    // write(test, sizeof(test));
     multiboot_info_t *mbinfo = (multiboot_info_t *) ebx;
     unsigned int address_of_module = mbinfo->mods_addr;
     call_module_t start_program = (call_module_t) address_of_module;
