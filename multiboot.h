252be0dfe7a30114df9db8e43309d9db30f968a4
gmmorgan1996 <griffin.mrogan@gmail.com>
2024-01-15 16:17:15 -0500

    test

--- multiboot.h
+++ multiboot.h
@@ -1,22 +1,21 @@
-/* multiboot.h - Multiboot header file. */
-/* Copyright (C) 1999,2003,2007,2008,2009,2010  Free Software Foundation, Inc.
+/* Copyright (C) 1999,2003,2007,2008,2009  Free Software Foundation, Inc.
  *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to
- *  deal in the Software without restriction, including without limitation the
- *  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
- *  sell copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- *  The above copyright notice and this permission notice shall be included in
- *  all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL ANY
- *  DEVELOPER OR DISTRIBUTOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- *  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
- *  IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL ANY
+ * DEVELOPER OR DISTRIBUTOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ * IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 #ifndef MULTIBOOT_HEADER
@@ -24,7 +23,6 @@
 
 /* How many bytes from the start of the file we search for the header. */
 #define MULTIBOOT_SEARCH                        8192
-#define MULTIBOOT_HEADER_ALIGN                  4
 
 /* The magic field should contain this. */
 #define MULTIBOOT_HEADER_MAGIC                  0x1BADB002
@@ -32,13 +30,16 @@
 /* This should be in %eax. */
 #define MULTIBOOT_BOOTLOADER_MAGIC              0x2BADB002
 
+/* The bits in the required part of flags field we don't support. */
+#define MULTIBOOT_UNSUPPORTED                   0x0000fffc
+
 /* Alignment of multiboot modules. */
 #define MULTIBOOT_MOD_ALIGN                     0x00001000
 
 /* Alignment of the multiboot info structure. */
 #define MULTIBOOT_INFO_ALIGN                    0x00000004
 
-/* Flags set in the ’flags’ member of the multiboot header. */
+/* Flags set in the 'flags' member of the multiboot header. */
 
 /* Align all boot modules on i386 page (4KB) boundaries. */
 #define MULTIBOOT_PAGE_ALIGN                    0x00000001
@@ -52,7 +53,7 @@
 /* This flag indicates the use of the address fields in the header. */
 #define MULTIBOOT_AOUT_KLUDGE                   0x00010000
 
-/* Flags to be set in the ’flags’ member of the multiboot info structure. */
+/* Flags to be set in the 'flags' member of the multiboot info structure. */
 
 /* is there basic lower/upper memory information? */
 #define MULTIBOOT_INFO_MEMORY                   0x00000001
@@ -86,12 +87,10 @@
 #define MULTIBOOT_INFO_APM_TABLE                0x00000400
 
 /* Is there video information? */
-#define MULTIBOOT_INFO_VBE_INFO                 0x00000800
-#define MULTIBOOT_INFO_FRAMEBUFFER_INFO         0x00001000
+#define MULTIBOOT_INFO_VIDEO_INFO               0x00000800
 
 #ifndef ASM_FILE
 
-typedef unsigned char           multiboot_uint8_t;
 typedef unsigned short          multiboot_uint16_t;
 typedef unsigned int            multiboot_uint32_t;
 typedef unsigned long long      multiboot_uint64_t;
@@ -156,6 +155,7 @@ struct multiboot_info
   /* Kernel command line */
   multiboot_uint32_t cmdline;
 
+
   /* Boot-Module list */
   multiboot_uint32_t mods_count;
   multiboot_uint32_t mods_addr;
@@ -190,42 +190,8 @@ struct multiboot_info
   multiboot_uint16_t vbe_interface_seg;
   multiboot_uint16_t vbe_interface_off;
   multiboot_uint16_t vbe_interface_len;
-
-  multiboot_uint64_t framebuffer_addr;
-  multiboot_uint32_t framebuffer_pitch;
-  multiboot_uint32_t framebuffer_width;
-  multiboot_uint32_t framebuffer_height;
-  multiboot_uint8_t framebuffer_bpp;
-#define MULTIBOOT_FRAMEBUFFER_TYPE_INDEXED 0
-#define MULTIBOOT_FRAMEBUFFER_TYPE_RGB     1
-#define MULTIBOOT_FRAMEBUFFER_TYPE_EGA_TEXT     2
-  multiboot_uint8_t framebuffer_type;
-  union
-  {
-    struct
-    {
-      multiboot_uint32_t framebuffer_palette_addr;
-      multiboot_uint16_t framebuffer_palette_num_colors;
-    };
-    struct
-    {
-      multiboot_uint8_t framebuffer_red_field_position;
-      multiboot_uint8_t framebuffer_red_mask_size;
-      multiboot_uint8_t framebuffer_green_field_position;
-      multiboot_uint8_t framebuffer_green_mask_size;
-      multiboot_uint8_t framebuffer_blue_field_position;
-      multiboot_uint8_t framebuffer_blue_mask_size;
-    };
-  };
 };
 typedef struct multiboot_info multiboot_info_t;
-
-struct multiboot_color
-{
-  multiboot_uint8_t red;
-  multiboot_uint8_t green;
-  multiboot_uint8_t blue;
-};
 
 struct multiboot_mmap_entry
 {
@@ -234,16 +200,13 @@ struct multiboot_mmap_entry
   multiboot_uint64_t len;
 #define MULTIBOOT_MEMORY_AVAILABLE              1
 #define MULTIBOOT_MEMORY_RESERVED               2
-#define MULTIBOOT_MEMORY_ACPI_RECLAIMABLE       3
-#define MULTIBOOT_MEMORY_NVS                    4
-#define MULTIBOOT_MEMORY_BADRAM                 5
   multiboot_uint32_t type;
 } __attribute__((packed));
 typedef struct multiboot_mmap_entry multiboot_memory_map_t;
 
 struct multiboot_mod_list
 {
-  /* the memory used goes from bytes ’mod_start’ to ’mod_end-1’ inclusive */
+  /* the memory used goes from bytes 'mod_start' to 'mod_end-1' inclusive */
   multiboot_uint32_t mod_start;
   multiboot_uint32_t mod_end;
 
@@ -254,20 +217,6 @@ struct multiboot_mod_list
   multiboot_uint32_t pad;
 };
 typedef struct multiboot_mod_list multiboot_module_t;
-
-/* APM BIOS info. */
-struct multiboot_apm_info
-{
-  multiboot_uint16_t version;
-  multiboot_uint16_t cseg;
-  multiboot_uint32_t offset;
-  multiboot_uint16_t cseg_16;
-  multiboot_uint16_t dseg;
-  multiboot_uint16_t flags;
-  multiboot_uint16_t cseg_len;
-  multiboot_uint16_t cseg_16_len;
-  multiboot_uint16_t dseg_len;
-};
 
 #endif /* ! ASM_FILE */
 
